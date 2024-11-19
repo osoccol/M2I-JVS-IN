4f830f07b9ce5f9563387dc91f79a8837de17b3f
osoccol <51447258+osoccol@users.noreply.github.com>
2024-11-19 15:52:00 +0100

    Chapitre 3 - PROGRAMMATION FONCTIONNELLE

--- crm.js
+++ crm.js
@@ -28,3 +28,41 @@ function updateClientInfo() {
   const infoClient = document.getElementById("infoClient");
   infoClient.textContent = `Nom : ${prenom}, Âge : ${age}, Pays : ${pays}`;
 }
+
+
+// CHAPITRE 3 - PROGRAMMATION FONCTIONNELLE 
+let clients = [client, {id: 102, nom: "Durand", email: "durand@example.com"}];
+console.log(clients);
+
+function ajouterClient(nouveauClient) {
+    clients.push(nouveauClient);
+    console.log("Client ajouté avec succès !", nouveauClient);
+}
+
+ajouterClient({id: 103, nom: "Martin", email: "martin@example.com"});
+console.log(clients);
+
+// const afficherNoms = (clients) => {
+//     console.log(clients.map(cli => cli.nom));
+// };
+// afficherNoms(clients);
+
+// const crm = {
+//     nom: "Gestion clients",
+//     afficherNom: function() {
+//         console.log(`CRM : ${this.nom}`);
+//     },
+//     afficherNomFleche: () => {
+//         console.log(`CRM : ${this.nom}`);
+//     },
+// }
+
+// crm.afficherNom();
+// crm.afficherNomFleche();
+
+// const rechercherClient = (idRecherche) => {
+//     return clients.find(cli => cli.id == idRecherche) || "Client introuvable";
+// }
+// console.log(rechercherClient(102));
+// console.log(rechercherClient(999));
+
